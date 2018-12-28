a927e546edf1ce6d96eac63e38a10cdf750c4f39
Brad <bstiff@iastate.edu>
2018-12-28 04:09:53 +0000

    started favicon

--- php/homepage.php
+++ php/homepage.php
@@ -8,9 +8,8 @@
 			$sql = "SELECT * FROM tiles ORDER BY tile_order";
 			global $conn;
 			$result = $conn->query($sql);
-			
-			$html = '<link rel="shortcut icon" href="#" />
-						<link href="/css/smarthome.css" rel="stylesheet"></link>
+
+			$html = '<link href="/css/smarthome.css" rel="stylesheet"></link>
 						<script type="text/javascript" src="/js/smarthome.js"></script>
 						<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 						<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
