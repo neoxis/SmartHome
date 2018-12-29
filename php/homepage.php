9b33bc81a4fd006a1586a905ef7bdbcbbe39341a
Brad <bstiff@iastate.edu>
2018-12-29 20:22:24 +0000

    font awesome 5 removed

--- php/homepage.php
+++ php/homepage.php
@@ -13,7 +13,6 @@
 						<script type="text/javascript" src="/js/smarthome.js"></script>
 						<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 						<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-						<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 						<body onload="startUp()">
 							<div class="modal">
 								<div class="modal-content"></div>
